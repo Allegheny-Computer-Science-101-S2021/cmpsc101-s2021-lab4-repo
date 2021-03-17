594487ca5efd6adfa97b2fca3838d189c5a95f79
amohan <amohan@amohanmacpro.local>
2021-03-17 12:10:45 -0400

    initial

--- starter-code/shape/shapestub.py
+++ starter-code/shape/shapestub.py
@@ -1,7 +1,10 @@
+# add your logic here ....
 from circle import circle 
+
 
 mycircle = circle(3.14)
 mycircle.area()
 mycircle.perimeter()
-print(str(round(mycircle.area, 2)),str(round(mycircle.perimeter, 2)))
+print(f"TESTING CIRCLE SHAPE\n---------------------------------")
+print(f"Circle area = {str(round(mycircle.area, 2))}\nCircle perimeter = {str(round(mycircle.perimeter, 2))}")
 
